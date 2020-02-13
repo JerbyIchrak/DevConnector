db7afa407834af770511d7d3fb57680dd7aae446
Jerby_Ichrak <ichrak.jerby@gmail.com>
2020-02-13 10:06:50 +0100

    Added getONe , update and remove routes for user model

--- models/users/userController.js
+++ models/users/userController.js
@@ -2,7 +2,7 @@ import User from "./User";
 import _ from "lodash";
 
 export async function create(req, res) {
-  console.log("********************************************");
+  console.log("********************Post************************");
   try {
     if (!req.body.email)
       return res.status(400).json({
@@ -31,6 +31,80 @@ export async function create(req, res) {
     });
   } catch (error) {
     console.log(error);
+    return res.status(500).end();
+  }
+}
+
+export async function getOne(req, res) {
+  try {
+    if (!req.params.id)
+      return res.status(400).json({
+        error: "id cannot be empty"
+      });
+
+    const user = await User.findById({
+      _id: req.params.id
+    });
+
+    return res.json(user);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).end();
+  }
+}
+
+export async function update(req, res) {
+  try {
+    if (!req.params.id)
+      return res.status(400).json({
+        error: "id cannot be empty"
+      });
+    if (!req.body.name)
+      return res.status(400).json({
+        error: "Name is required !"
+      });
+    if (!req.body.email)
+      return res.status(400).json({
+        error: "email is required !"
+      });
+    if (!req.body.password)
+      return res.status(400).json({
+        error: "password is required !"
+      });
+
+    const user = await User.findOne({
+      _id: req.params.id
+    });
+    if (!user)
+      return res.status(401).json({
+        error: "user not found !"
+      });
+
+    user.name = req.body.name;
+    user.email = req.body.email;
+    user.password = req.body.password;
+
+    await user.save();
+
+    await User.update({ _id: req.params.id }, { $set: user });
+
+    return res.status(200).end();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).end();
+  }
+}
+
+export async function remove(req, res) {
+  try {
+    if (!req.params.id) return res.status(400).end();
+
+    await User.remove({
+      _id: req.params.id
+    });
+
+    return res.status(204).end();
+  } catch (error) {
     return res.status(500).end();
   }
 }
--- models/users/usersRoutes.js
+++ models/users/usersRoutes.js
@@ -1,5 +1,8 @@
-import { create } from "./userController";
+import { create, getOne, update, remove } from "./userController";
 
 export default function(router) {
   router.post("/register", create);
+  router.get("/getUser/:id", getOne);
+  router.put("/updateUser/:id", update);
+  router.delete("/deleteUser/:id", remove);
 }
